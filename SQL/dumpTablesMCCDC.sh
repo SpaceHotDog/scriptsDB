79947843f2d51a8e39033463bd5487a254b0c153
SpaceHotDog <nicolassolignac@gmail.com>
2019-08-01 10:15:53 -0300

    Added - Scripts DB's migrated from Bash Scripts Repo

---
+++ SQL/dumpTablesMCCDC.sh
@@ -0,0 +1,3 @@
+mysqldump -unsolignac -p mc_demo company company_setting strategy operator extra_field labelcanned_category canned_message > mc_demo_tables.sql
+
+mysqldump -unsolignac -p mc_colas_demo operator supervisor topic topic_opertor topic_supervisor > mc_cloas_demo_tables.sql
