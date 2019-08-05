b586ce57500a49a54da21b404748d610808c44cf
SpaceHotDog <nicolassolignac@gmail.com>
2019-08-05 12:45:24 -0300

    Modified - mariaDBAllDBIndexes.sql - SELECT INTO OUTFILE

--- SQL/mariaDBAllDBIndexes.sql
+++ SQL/mariaDBAllDBIndexes.sql
@@ -6,7 +6,13 @@ SELECT index_schema,
             WHEN 1 THEN 'Not Unique'
             ELSE 'Unique'
             END AS is_unique,
-        table_name
+            /*
+            REMEMBER TO SPECIFY PATH: INTO OUTFILE 'PATH-TO-SAVE-FILE'
+            */
+        table_name INTO OUTFILE '/tmp/mc_demo-Indexes.csv'
+/* CSV FORMAT */
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
 FROM information_schema.statistics
 /*
 REMEMBER TO SPECIFY DB NAME: WHERE table_schema = 'db_name'
