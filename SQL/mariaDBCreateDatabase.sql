33625f6269f774e96c9e51b83b7aed17709ca7d3
SpaceHotDog <nicolassolignac@gmail.com>
2019-08-05 22:07:57 -0300

    Added - Create Database .sql

---
+++ SQL/mariaDBCreateDatabase.sql
@@ -0,0 +1,7 @@
+/*
+SOURCES:
+- CREATE DB: https://mariadb.com/kb/en/library/create-database/
+- COLLATION AND CHARACTER SET: https://mariadb.com/kb/en/library/setting-character-sets-and-collations/
+*/
+
+CREATE DATABASE mc_colas CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
