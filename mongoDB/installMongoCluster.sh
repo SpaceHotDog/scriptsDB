be436d51352bdbe25bee72c1217def057f9a3cc3
SpaceHotDog <nicolassolignac@gmail.com>
2019-08-12 15:53:29 -0300

    Modified - installMongoCluster.sh - Comments

--- mongoDB/installMongoCluster.sh
+++ mongoDB/installMongoCluster.sh
@@ -4,3 +4,14 @@
 
 # Definimos algunas variables
 OSV=$(cat /etc/os-release)
+GPGKey='https://www.mongodb.org/static/pgp/server-3.2.asc'
+
+
+# Agregamos la Key GPG
+wget -qO - $GPGKey | sudo apt-key add -
+
+# Agregamos el repo a la lista de apt
+echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.2 main" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+# Actualizamos los repos e instalamos Mongo
+apt-get update && apt-get install -y mongodb-org
