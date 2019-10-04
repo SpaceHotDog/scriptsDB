2c14d060621fd285819b099e43d4dc21c68490be
SpaceHotDog <nicolassolignac@gmail.com>
2019-10-04 11:47:57 -0300

    Modified - Script Celulares 3 - TODO List

--- mariaDBDumpCSVFormat.sh
+++ mariaDBDumpCSVFormat.sh
@@ -6,6 +6,10 @@
 # https://www.computerhope.com/unix/usplit.htm
 # https://www.mkyong.com/linux/linux-how-to-gzip-a-folder/
 
+### TODO ###
+# OFFSET: El offset hace que no se registren los datos de las ultimas 3 tablas (empresa, nrolocalidad, nroprovincia) - !!DONE
+# NAMING: Guardar los csv.gz usando tambien el nombre de la tabla (Variable $TBL) - !!DONE
+
 # Creamos y nos movemos al entorno de trabajo
 export DB="celulares_2019"
 export DATE=`date +%Y%m%d`
@@ -21,6 +25,7 @@ export USER="celulares_admin"
 export PWD="celuser.3135"
 export N=0
 export OFFSET=0
+export OFFSETTMP=0
 
 # Comenzamos el DUMP
 echo "Comenzamos el proceso" >> $LOGFILE
@@ -34,17 +39,28 @@ for TBL in $(mysql -h$DBHOST -u$USER -p$PWD $DB -sN -e "SHOW TABLES;"); do
     echo "Comenzamos el dump de la tabla: '$TBL'" >> $LOGFILE
 
     while [ $COUNTER -ne 0 ]; do
-        mysql -B -h$DBHOST -u$USER -p$PWD -e "SELECT * FROM $TBL LIMIT 1000000 OFFSET $OFFSET;" $DB | sed "s/\"/\"\"/g;s/'/\'/;s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g" | gzip > $DB'-'$DATE'-'$OFFSET.csv.gz
+        if [ $TBL = 'celulares' ]
+        then
+            let OFFSETTMP=$OFFSET
+            OFFSET=$OFFSETTMP
+        else
+            let OFFSET=0
+        fi
 
-        echo "Fin del Dump de la tabla: '$TBL' - Iteracion Num: $COUNTER - OFFSET: $OFFSET" >> $LOGFILE
+        mysql -B -h$DBHOST -u$USER -p$PWD -e "SELECT * FROM $TBL LIMIT 1000000 OFFSET $OFFSET;" $DB | sed "s/\"/\"\"/g;s/'/\'/;s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g" | gzip > $DB'-'$DATE'-'$TBL'-'$OFFSET.csv.gz
+
+        echo "Fin del Dump de la tabla: '$TBL' - Clicos restantes: $COUNTER - OFFSET: $OFFSET" >> $LOGFILE
+
         let COUNTER-=1
         let OFFSET+=1000000
     done
 
     let N+=1
     echo "Fin del dump de la tabla: '$TBL' - Vuelta $N" >> $LOGFILE
+
 done
 
 # Comprimimos los archivos
+echo "Comprimimos la carpeta WORKDIR: '$WORKDIR'" >> $LOGFILE
 cd ..
 tar -zcvf $WORKDIR.tar.gz $WORKDIR/
